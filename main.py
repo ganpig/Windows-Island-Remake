3cc1a728f326d4839fb0b5eb0b5b556902c87df2
ganpig <ganpig@qq.com>
2022-11-05 17:25:45 +0800

    upd

--- main.py
+++ main.py
@@ -49,20 +49,20 @@ class Window(tk.Tk):
     def __init__(self):
         super().__init__()
         self.overrideredirect(True)  # 设置窗口无边框
-        self.attributes('-alpha', 0.5)  # 设置窗口半透明
         self.attributes('-topmost', True)  # 设置窗口始终位于顶部
-        self.geometry('%dx%d+%d+%d' % (WINDOW_WIDTH, BUTTON_HEIGHT*2, (self.winfo_screenwidth()-WINDOW_WIDTH)/2, BUTTON_HEIGHT+10))  # 设置窗口大小
         self.config(background='black')  # 设置框架黑色
         self.extended = False  # 是否展开,初始为没有
+        self.extend_time = 0  # 上一次展开操作的时间
+        self.pos_x = (self.winfo_screenwidth()-WINDOW_WIDTH)//2
+        self.pos_y = BUTTON_HEIGHT+10
 
         # 显示时间和内存
         self.time_memory_label = tk.Label(self, background='black', foreground='white')  # 显示时间的标签
         self.time_memory_label.place(x=0, y=0, width=WINDOW_WIDTH-SMALL_BUTTON_WIDTH, height=BUTTON_HEIGHT*2)
-        self.get_time_memory()  # 获取时间和内存
 
         # 时间标签可以拖动
-        self.time_memory_label.bind('<Button-1>', self.set_pos)
-        self.time_memory_label.bind('<B1-Motion>', self.move)
+        self.time_memory_label.bind('<Button-1>', self.on_click)
+        self.time_memory_label.bind('<B1-Motion>', self.on_move)
 
         # 退出按钮
         self.quit_button = Button(master=self, text='×', command=lambda x: self.destroy())
@@ -80,26 +80,31 @@ class Window(tk.Tk):
         for i, (name, command) in enumerate(TOOL_LIST):
             CmdButton(self.tool_frame, name, command, i % 2, i//2)
 
-    def get_time_memory(self):  # 获取时间和内存
+        self.after(10, self.update_state)
+
+    def update_state(self):  # 刷新窗口状态
         mem = psutil.virtual_memory()
         total = float(mem.total)/1024**3
         used = float(mem.used)/1024**3
         self.time_memory_label.config(text=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())+f'\n内存: {used:.2f}G / {total:.2f}G')  # 更改显示的时间文字
-        self.after(1000, self.get_time_memory)  # 一秒后再次执行此函数
+        now_time = time.time()
+        animation_prog = (min(0.3, now_time-self.extend_time) if self.extended else max(0, 0.3-(now_time-self.extend_time)))/0.3
+        self.geometry(f'{WINDOW_WIDTH}x{int(BUTTON_HEIGHT*2+animation_prog*self.tool_frame.winfo_height())}+{self.pos_x}+{self.pos_y}')
+        self.attributes('-alpha', 0.5+animation_prog*0.3)  # 设置窗口透明度
+        self.after(10, self.update_state)
 
-    def move(self, event):  # 移动事件
-        new_x = self.winfo_x()+event.x-self.click_x
-        new_y = self.winfo_y()+event.y-self.click_y
-        self.geometry('%dx%d+%d+%d' % (WINDOW_WIDTH, BUTTON_HEIGHT*2+self.extended*self.tool_frame.winfo_height(), new_x, new_y))
-
-    def set_pos(self, event):  # 设置点击位置
+    def on_click(self, event):  # 点击事件
         self.click_x, self.click_y = event.x, event.y
 
+    def on_move(self, event):  # 移动事件
+        self.pos_x = self.winfo_x()+event.x-self.click_x
+        self.pos_y = self.winfo_y()+event.y-self.click_y
+
     def extend(self, event):  # 展开
+        now_time = time.time()
+        self.extend_time = now_time-(0.3-(now_time-self.extend_time)) if now_time-self.extend_time < 0.3 else now_time
         self.extended = not self.extended
-        self.attributes('-alpha', 0.8 if self.extended else 0.5)  # 设置窗口透明度
-        self.extend_button.config(text='↑' if self.extended else '↓')
-        self.geometry('%dx%d' % (WINDOW_WIDTH, BUTTON_HEIGHT*2+self.extended*self.tool_frame.winfo_height()))
+        self.extend_button.config(text='↓↑'[self.extended])
 
 
 if __name__ == '__main__':
